662e45304924e8d5b6854d4a803a9d0fc480d3d3
kuan <cugkuan@163.com>
2025-10-22 19:27:55 +0800

    Upgrade(Plugin)：更改Process 的  gradle 配置

--- process/build.gradle.kts
+++ process/build.gradle.kts
@@ -1,96 +1,58 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.jetbrains.kotlin.jvm)
     alias(libs.plugins.ksp)
-    `maven-publish`
-    `signing`
-    id("java")
+    alias(libs.plugins.publish)
 }
+java {
+    withSourcesJar()
+   // withJavadocJar()
+}
+
 dependencies {
     implementation(kotlin("stdlib")) // Kotlin 标准库
     implementation(libs.symbol.processing.api) // KSP API
     implementation(libs.gson)
     compileOnly(libs.kotlin.compiler.embeddable) // Kotlin compiler
 }
-// 配置 Java 兼容性
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
-
 // 配置 Kotlin 编译目标
 kotlin {
     jvmToolchain(11) // 使用 JDK 11
 }
+group = "top.brightk"
+version = "0.0.5.0"
 
-//try {
-//    apply(from = "${rootProject.rootDir.parent}/gradle/publish.gradle.kts")
-//}catch (e:Exception){
-//    apply(from = "${rootDir}/gradle/publish.gradle.kts")
-//}
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+    coordinates("top.brightk", "bridge-ksp",
+        project.version.toString())
 
-////java {
-////    withJavadocJar()
-////    withSourcesJar()
-////}
-//
-//tasks.register<Jar>("sourcesJar") {
-//    archiveClassifier.set("sources")
-//    from(sourceSets.main.get().allSource)
-//}
-//tasks.register<Jar>("javadocJar") {
-//    archiveClassifier.set("javadoc")
-//    from(tasks.getByName("javadoc"))
-//}
-//
-//
-//
-//group = "top.brightk"
-//version = "0.0.4.1"
-//
-//
-//val uploadRepository: Action<RepositoryHandler> by extra
-//publishing {
-//    publications {
-//        create<MavenPublication>("release") {
-//            artifactId = "bridge-ksp"
-//            groupId = project.group.toString()
-//            version = project.version.toString()
-//            from(components["kotlin"])
-//
-//            artifact(tasks["sourcesJar"]) // Add sources JAR
-//            artifact(tasks["javadocJar"]) // Add Javadoc JAR
-//
-//            pom {
-//                // public maven must
-//                name.set("Bridge ksp")
-//                description.set("Bridge ksp,work for Bridge;Ksp插件,负责自动化注册")
-//                val pomUrl = "https://github.com/cugkuan/Bridge"
-//                val pomScm = "https://github.com/cugkuan/Bridge.git"
-//                url.set(pomUrl)
-//                licenses {
-//                    license {
-//                        name.set("The Apache Software License, Version 2.0")
-//                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-//                    }
-//                }
-//                developers {
-//                    developer {
-//                        id.set("BrightK")
-//                        name.set("BrightK")
-//                        email.set("cugkuan@163.com")
-//                    }
-//                }
-//                scm {
-//                    connection.set(pomUrl)
-//                    developerConnection.set(pomScm)
-//                    url.set(pomUrl)
-//                }
-//            }
-//        }
-//    }
-//    repositories(uploadRepository)
-//}
-//
-//signing {
-//    sign(publishing.publications["release"])
-//}
+    pom {
+        // public maven must
+        name.set("Bridge ksp")
+        description.set("Bridge ksp,work for Bridge;Ksp插件,负责自动化注册")
+        val pomUrl = "https://github.com/cugkuan/Bridge"
+        val pomScm = "https://github.com/cugkuan/Bridge.git"
+        url.set(pomUrl)
+        licenses {
+            license {
+                name.set("The Apache Software License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("BrightK")
+                name.set("BrightK")
+                email.set("cugkuan@163.com")
+            }
+        }
+        scm {
+            connection.set(pomUrl)
+            developerConnection.set(pomScm)
+            url.set(pomUrl)
+        }
+    }
+}
